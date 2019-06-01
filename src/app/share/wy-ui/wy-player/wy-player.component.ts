b848eeebeb4472d607e076d7b73ddd728f9ddc38
lycHub <1120805364@qq.com>
2019-06-01 22:25:49 +0800

    日常提交

--- src/app/share/wy-ui/wy-player/wy-player.component.ts
+++ src/app/share/wy-ui/wy-player/wy-player.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {SongSheetList} from "../../../service/song/song.service";
 
 @Component({
@@ -6,42 +6,117 @@ import {SongSheetList} from "../../../service/song/song.service";
   templateUrl: './wy-player.component.html',
   styleUrls: ['./wy-player.component.less']
 })
-export class WyPlayerComponent implements OnInit, OnChanges {
+export class WyPlayerComponent implements OnInit, OnChanges, AfterViewInit {
   arr = Array(100).fill(3);
   @Input() songSheetList: SongSheetList[];
   
   // 是否可以播放
   private songReady = false;
   
-  // 正在播放
-  currentSong: SongSheetList;
+  // 播放状态vuex
+  private playing = false;
+  
+  // 当前播放的索引vuex
+  currentIndex: number;
+  
+  
+  // 正在播放vuex
+  get currentSong(): SongSheetList | null {
+    return this.songSheetList[this.currentIndex];
+  }
+  
+  // 播放时间
+  currentTime: number;
+  
+  // 总时长
+  duration: number;
+  
+  // 播放进度（百分比）
+  get percent(): number | null {
+    return (this.currentTime / this.duration) * 100;
+  }
   
   @ViewChild('audio') private audio: ElementRef;
-  constructor() { }
+  private audioEl: HTMLAudioElement;
   ngOnInit() {}
+  
+  ngAfterViewInit(): void {
+    this.audioEl = this.audio.nativeElement;
+  }
+  
+  onChange(e) {
+    console.log('e', e);
+  }
   
   ngOnChanges(changes: SimpleChanges): void {
     const songSheetList = changes.songSheetList;
     if (songSheetList && songSheetList.currentValue.length) {
-      this.currentSong = songSheetList.currentValue[0];
+      this.currentIndex = 0;
     }
   }
   
   
-  play() {
+  onToggle() {
+    console.log(this.songReady);
+    if (this.songReady) {
+      console.log('aa');
+      this.playing = !this.playing;
+      if (this.playing) {
+        this.audioEl.play();
+      }else {
+        this.audioEl.pause();
+      }
+    }
+  }
+  
+  onPrev(index: number) {
+    if (this.songReady) {
+      this.currentIndex = index < 0 ? this.songSheetList.length - 1 : index;
+      this.songReady = false;
+    }
+  }
+  onNext(index: number) {
+    if (this.songReady) {
+      this.currentIndex = index >= this.songSheetList.length ? 0 : index;
+      this.songReady = false;
+    }
+  }
+  
+  private play() {
+    this.audioEl.play();
+    this.duration = this.audioEl.duration;
+    this.playing = true;
+  }
+  
+  // 缓存一定数据足以播放时触发
+  onCanPlay() {
     this.songReady = true;
+    this.play();
   }
   
   
-  updateTime(e) {
-    // console.log('currentTime', e);
+  onTimeUpdate(e) {
+    this.currentTime = e.target.currentTime;
   }
   
-  error() {
-    this.songReady = false;
+  private formatTime(time: number): string {
+    if (time) {
+      const temp = time | 0;
+      const minute = temp / 60 | 0;
+      const second = (temp % 60).toString().padStart(2, '0');
+      return `${minute}:${second}`;
+    }else {
+      return '00:00';
+    }
   }
   
-  end() {
+  onError() {
+    if (this.currentSong) {
+      this.songReady = true;
+    }
+  }
+  
+  onEnded() {
     console.log('end');
   }
 }
--- src/app/share/wy-ui/wy-player/wy-player.module.ts
+++ src/app/share/wy-ui/wy-player/wy-player.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WyPlayerComponent } from './wy-player.component';
 import { WySliderModule } from '../wy-slider/wy-slider.module';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     WySliderModule
   ],
   declarations: [WyPlayerComponent],
