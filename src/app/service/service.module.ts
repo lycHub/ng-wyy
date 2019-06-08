42c612b38b62f11047eb3ec49317978b4bc221b4
lycHub <1120805364@qq.com>
2019-06-08 16:32:08 +0800

    日常提交

--- src/app/service/service.module.ts
+++ src/app/service/service.module.ts
@@ -1,12 +1,11 @@
 import {NgModule, PLATFORM_ID} from '@angular/core';
-import {NzIconService} from "ng-zorro-antd/icon";
 import {API_CONFIG, WINDOW} from "../core/inject-tokens";
 import {isPlatformBrowser} from "@angular/common";
 
 @NgModule({
   providers: [
     {
-      provide: API_CONFIG, useValue: 'http://127.0.0.1:3000/'
+      provide: API_CONFIG, useValue: 'http://127.0.0.1:9000/'
     },
     /*{ provide: ICON_FONT,
       useFactory(_iconService: NzIconService) {
