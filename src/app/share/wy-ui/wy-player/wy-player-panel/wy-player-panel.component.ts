336ab2ac6bb676dcdd25bfa4f9817e0912ad6827
lycHub <1120805364@qq.com>
2019-06-08 12:12:41 +0800

    日常提交

--- src/app/share/wy-ui/wy-player/wy-player-panel/wy-player-panel.component.ts
+++ src/app/share/wy-ui/wy-player/wy-player-panel/wy-player-panel.component.ts
@@ -1,5 +1,13 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {
+  AfterViewInit, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges,
+  ViewChild
+} from '@angular/core';
 import {SongList} from "../../../../service/song/song.service";
+import BScroll from '@better-scroll/core';
+import MouseWheel from '@better-scroll/mouse-wheel';
+import ScrollBar from '@better-scroll/scroll-bar';
+BScroll.use(MouseWheel);
+BScroll.use(ScrollBar);
 
 @Component({
   selector: 'app-wy-player-panel',
@@ -16,15 +24,34 @@ export class WyPlayerPanelComponent implements OnInit, OnChanges, AfterViewInit 
   
   // 切歌
   @Output() readonly onChangeSong = new EventEmitter<SongList>();
+  
+  @ViewChild('listWrap', { static: true }) private listWrapRef: ElementRef;
+  private songListRefs: NodeList;
+  
   private currentIndex: number;
-  constructor() { }
+  
+  private bs: BScroll;
+  constructor() {
+    // console.log('constructor');
+  }
 
   ngOnInit() {
+    // console.log('init');
   }
   
   
   ngAfterViewInit(): void {
-  
+    const wrap = this.listWrapRef.nativeElement;
+    this.bs = new BScroll(wrap, {
+      click: true,
+      scrollbar: {
+        fade: false,
+        interactive: true
+      },
+      mouseWheel: {}
+    });
+    this.songListRefs = wrap.querySelectorAll('ul li');
+    this.scrollToElement();
   }
   
   ngOnChanges(changes: SimpleChanges): void {
@@ -32,6 +59,18 @@ export class WyPlayerPanelComponent implements OnInit, OnChanges, AfterViewInit 
     if (changes['currentSong']) {
       // console.log(changes['currentSong'].currentValue);
       this.currentIndex = this.songList.findIndex(item => item.id === changes['currentSong'].currentValue.id);
+      this.scrollToElement();
+    }
+  }
+  
+  private scrollToElement() {
+    if (this.songListRefs) {
+      const dom = <HTMLElement>this.songListRefs[this.currentIndex];
+      const offsetTop = dom.offsetTop;
+      const scrollY = this.bs.y;
+      if ((offsetTop < Math.abs(scrollY)) || (Math.abs(offsetTop - Math.abs(scrollY)) > 205)) {
+        this.bs.scroller.scrollToElement(dom, 300, false, false);
+      }
     }
   }
 
