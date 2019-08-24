f130c77f06207bed273b37c86d73f0242d60d692
lycHub <1120805364@qq.com>
2019-08-24 19:16:54 +0800

    over

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from 'src/app/services/home.service';
+import { Banner } from '../../services/data-types/common.types';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,11 @@ import { HomeService } from 'src/app/services/home.service';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
+  banners: Banner[];
 
   constructor(private homeServe: HomeService) {
     this.homeServe.getBanners().subscribe(banners => {
-      console.log('banners :', banners);
+      this.banners = banners;
     });
   }
 
