38cc2b1eb1a592a8e58b281aa928173da3655e2c
lycHub <1120805364@qq.com>
2019-09-15 10:44:27 +0800

    over

--- src/app/pages/song-info/song-info.component.ts
+++ src/app/pages/song-info/song-info.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/internal/operators';
+import { map, takeUntil } from 'rxjs/internal/operators';
 import { BaseLyricLine, WyLyric } from '../../share/wy-ui/wy-player/wy-player-panel/wy-lyric';
 import { Song } from 'src/app/services/data-types/common.types';
+import { SongService } from '../../services/song.service';
+import { Subject } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { AppStoreModule } from 'src/app/store';
+import { BatchActionsService } from 'src/app/store/batch-actions.service';
+import { NzMessageService } from 'ng-zorro-antd';
+import { getCurrentSong } from 'src/app/store/selectors/player.selector';
 
 @Component({
   selector: 'app-song-info',
   templateUrl: './song-info.component.html',
   styleUrls: ['./song-info.component.less']
 })
-export class SongInfoComponent implements OnInit {
+export class SongInfoComponent implements OnInit, OnDestroy {
   song: Song;
   lyric: BaseLyricLine[];
 
@@ -19,14 +26,30 @@ export class SongInfoComponent implements OnInit {
     iconCls: 'down'
   }
 
-  constructor(private route: ActivatedRoute) {
+  currentSong: Song;
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private route: ActivatedRoute,
+    private songServe: SongService,
+    private store$: Store<AppStoreModule>,
+    private batchActionServe: BatchActionsService,
+    private nzMessageServe: NzMessageService
+  ) {
     this.route.data.pipe(map(res => res.songInfo)).subscribe(([song, lyric]) => {
       this.song = song;
       this.lyric = new WyLyric(lyric).lines;
+      this.listenCurrent();
     });
   }
 
   ngOnInit() {
+  }
+
+  private listenCurrent() {
+    this.store$
+    .pipe(select('player'), select(getCurrentSong), takeUntil(this.destroy$))
+    .subscribe(song => this.currentSong = song);
   }
 
   toggleLyric() {
@@ -38,5 +61,24 @@ export class SongInfoComponent implements OnInit {
       this.controlLyric.label = '展开';
       this.controlLyric.iconCls = 'down';
     }
+  }
+
+  onAddSong(song: Song, isPlay = false) {
+    if (!this.currentSong || this.currentSong.id !== song.id) {
+      this.songServe.getSongList(song)
+      .subscribe(list => {
+        if (list.length) {
+          this.batchActionServe.insertSong(list[0], isPlay);
+        }else {
+          this.nzMessageServe.create('warning', '无url!');
+        }
+      });
+    }
+  }
+
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
